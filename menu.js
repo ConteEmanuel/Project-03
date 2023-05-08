abe6bbe03f77f3c50845c87e44efc28ea7b95e3a
Fenush <econtegarcia@gmail.com>
2023-05-08 03:08:19 -0300

    rightClick + menuBuilder Done

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 const arrayMenu =["paint","change","more",] // from menuBuilder
 const menuContainer = document.createElement('div') // from menuBuilder
 
+////////////////////
 function menuBuilder()
 {document.body.append(menuContainer) //Menu added
 menuContainer.classList.add("menu")
-
 arrayMenu.forEach(element => { //Button added
 const button = document.createElement('div')
 menuContainer.appendChild(button)
@@ -14,14 +14,17 @@ if (element == arrayMenu[1]){
 }
 button.setAttribute('id', (element+"Menu"))
 button.classList.add("button")
-});}
-
+});
+}
+/////////////////
 function rightClickMenu (){
     window.oncontextmenu = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         menuContainer.style.left = e.pageX+"px";
         menuContainer.style.top = e.pageY+"px";
-        menuBuilder();
+//        menuBuilder();
 }
 }
-rightClickMenu()
+
+menuBuilder();
+rightClickMenu();
