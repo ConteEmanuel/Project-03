45793e8042cb5ba4e12f669ea510bcf962782293
Fenush <econtegarcia@gmail.com>
2023-05-08 03:48:55 -0300

    Comments Updated

--- menu.js
+++ menu.js
@@ -1,30 +1,28 @@
-const arrayMenu =["paint","change","more",] // from menuBuilder
-const menuContainer = document.createElement('div') // from menuBuilder
+const menuButtons = ["paint", "change", "more"]; // from menuBuilder
+const menuContainer = document.createElement("div"); // from menuBuilder
 
-////////////////////
-function menuBuilder()
-{document.body.append(menuContainer) //Menu added
-menuContainer.classList.add("menu")
-arrayMenu.forEach(element => { //Button added
-const button = document.createElement('div')
-menuContainer.appendChild(button)
-button.textContent=element
-if (element == arrayMenu[1]){
-    button.classList.add("secondButton");
+function menuBuilder() {    //Menu Structure Builder
+    document.body.append(menuContainer);
+    menuContainer.classList.add("menu");
+    menuButtons.forEach((element) => {  // Button Builder
+        const button = document.createElement("div");
+        menuContainer.appendChild(button);
+        button.textContent = element;
+        if (element == menuButtons[1]) { // Set a Center bigger button
+            button.classList.add("centerButton");
+    }
+    button.setAttribute("id", element + "Menu"); //id for future use
+    button.classList.add("button");
+    });
 }
-button.setAttribute('id', (element+"Menu"))
-button.classList.add("button")
-});
-}
-/////////////////
-function rightClickMenu (){
+
+function rightClickMenu() { // set cords for menu
     window.oncontextmenu = (e) => {
         e.preventDefault();
-        menuContainer.style.left = e.pageX+"px";
-        menuContainer.style.top = e.pageY+"px";
-//        menuBuilder();
+        menuContainer.style.left = e.pageX + "px";
+        menuContainer.style.top = e.pageY + "px";
+    };
 }
-}
-menuContainer.style.left="-100px";
+menuContainer.style.left = "-100px"; //Hide menu at load
 menuBuilder();
 rightClickMenu();
