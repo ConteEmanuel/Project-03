b4d642ac64131c3992e3b4ba32a855a6fb0db8c6
Fenush <econtegarcia@gmail.com>
2023-05-09 06:05:55 -0300

    WhiteorBlack() Comments Added

--- buttons.js
+++ buttons.js
@@ -1,13 +1,13 @@
 function whiteOrBlack() {
-    const whiteButton = document.getElementById("whiteButton");
-    whiteButton.onclick = (e) => {
-        if (cursorColor == "black") {
-            cursorColor = "white";
-            whiteButton.textContent="black";
-        } else {
-            cursorColor = "black";
-            whiteButton.textContent="white";
-        }
-    menuContainer.classList.toggle("none");
-    };
+  const whiteButton = document.getElementById("whiteButton"); //build button from menuArray[0]
+  whiteButton.onclick = (e) => {    //set next cursor state and next button text
+    if (cursorColor == "black") {
+      cursorColor = "white";
+      whiteButton.textContent = "black";
+    } else {
+      cursorColor = "black";
+      whiteButton.textContent = "white";
+    }
+    menuContainer.classList.toggle("none"); //close menu
+  };
 }
