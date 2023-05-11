8e37a8e7290c2d85496d5b051da51a1437a37099
Fenush <econtegarcia@gmail.com>
2023-05-10 22:16:44 -0300

    Instruction Variable Added

--- variables.js
+++ variables.js
@@ -3,8 +3,9 @@ const menuButtons = ["white", "change", "grid"];
 const menuContainer = document.createElement("div");
 //from grid.js
 const cellContainer = document.createElement("div");
+const instructions = document.createElement('div');
 let cellRow = 16,
   cell = "",
-  cursorColor = "";
+  cursorColor = "black";
 //from buttons.js
 let randomColor;
