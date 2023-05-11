8c293014571e26e62deb16aa04e32361f887f32c
Fenush <econtegarcia@gmail.com>
2023-05-11 16:30:41 -0300

    initialColor var Added & MenuButton[1] named Change is now Random

--- variables.js
+++ variables.js
@@ -1,11 +1,13 @@
 // from menu.js
-const menuButtons = ["white", "change", "grid"];
+const menuButtons = ["white", "random", "grid"];
 const menuContainer = document.createElement("div");
 //from grid.js
 const cellContainer = document.createElement("div");
-const instructions = document.createElement('div');
+const instructions = document.createElement("div");
+let initialWhiteColor = 1;
 let cellRow = 16,
   cell = "",
-  cursorColor = "black";
+  cursorColor = "white";
 //from buttons.js
 let randomColor;
+let lastColor;
